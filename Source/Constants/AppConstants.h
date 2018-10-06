618fa5de09c26c4e298d90664d992fecb3e245d2
bill-auger <mr.j.spam.me@gmail.com>
2018-10-06 03:20:40 -0400

    initial Audio/GUI boilerplate

---
+++ Source/Constants/AppConstants.h
@@ -0,0 +1,15 @@
+#pragma once
+
+
+#include "../../JuceLibraryCode/JuceHeader.h"
+
+
+/** the APP class defines configuration and runtime constants
+        pertaining to the core application and business logic */
+class APP
+{
+public:
+
+  // names and IDs
+  static const String APP_NAME ;
+} ;
